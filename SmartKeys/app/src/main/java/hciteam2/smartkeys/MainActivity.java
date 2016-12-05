6cbe667a961900ea6f606186c8c5adfdbf6775e5
Khanh Le Son <lskhanh@MacBook-Air-de-Khanh.local>
2016-12-05 13:16:18 +0900

    Interface updated

--- SmartKeys/app/src/main/java/hciteam2/smartkeys/MainActivity.java
+++ SmartKeys/app/src/main/java/hciteam2/smartkeys/MainActivity.java
@@ -4,29 +4,47 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.GridView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.os.AsyncTask;
-
+import android.content.SharedPreferences;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Switch;
+import android.widget.PopupWindow;
 
+import android.content.Context;
+import android.content.DialogInterface;
+
+import android.view.LayoutInflater;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    final Context context = this;
+    public Switch switch_mode;
     private boolean editing = true;
     private LinkedList<ButtonItem> coordinates;
     private TCPClient mTcpClient;
@@ -200,4 +218,101 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+
+    //Menu-main
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main,menu);//Menu Resource, Menu
+
+        SharedPreferences settings = getSharedPreferences("settings", 0);
+        boolean isChecked = settings.getBoolean("checkbox", false);
+        MenuItem item1 = menu.findItem(R.id.action_check);
+        MenuItem item2 = menu.findItem(R.id.action_check2);
+        item1.setChecked(isChecked);
+        item2.setChecked(isChecked);
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+
+                // get prompts.xml view
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.prompts, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.editTextDialogUserInput);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit text
+                                        //result.setText(userInput.getText());
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+                //Toast.makeText(getApplicationContext(),"Item 1 Selected",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_check:
+                item.setChecked(!item.isChecked());
+                SharedPreferences settings = getSharedPreferences("settings", 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("checkbox", item.isChecked());
+                editor.commit();
+
+                if(item.isChecked()){
+                    Toast.makeText(getApplicationContext(),"Edit mode is currently ON",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Edit mode is currently OFF",Toast.LENGTH_LONG).show();
+                }
+                return true;
+
+            case R.id.action_check2:
+                item.setChecked(!item.isChecked());
+                SharedPreferences settings2 = getSharedPreferences("settings", 0);
+                SharedPreferences.Editor editor2 = settings2.edit();
+                editor2.putBoolean("checkbox", item.isChecked());
+                editor2.commit();
+
+                if(item.isChecked()){
+                    Toast.makeText(getApplicationContext(),"Rearrange mode is currently ON",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Rearrange mode is currently OFF",Toast.LENGTH_LONG).show();
+                }
+
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // Display popup attached to the button as a position anchor
+
 }
